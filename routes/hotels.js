4b67cc192b91794a9b672fa94a7bdd1ba1daa0a3
Maheshwar2802 <maheshwar.goli@gmail.com>
2021-11-02 21:14:52 -0500

    fixed the error

--- routes/hotels.js
+++ routes/hotels.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('Hotels', { title: 'Hotels Search Results' });
+    res.render('hotels', { title: 'Hotels Search Results' });
 });
 
 module.exports = router;
